70b584235eb853c52cf39bc059eb3921581120f5
Konate beh <konatebeh20@gmail.com>
2024-09-19 14:52:50 +0000

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,3 +1,4 @@
+# pip install sqlalchemy pymysql
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Enum, Date, DECIMAL
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
